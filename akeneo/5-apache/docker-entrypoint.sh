d20fd9a5590de46d8b38ed6dc5901a4fa13e493d
djalal <hi@djal.al>
2021-05-23 19:22:21 +0200

    couple fixes

--- akeneo/5-apache/docker-entrypoint.sh
+++ akeneo/5-apache/docker-entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
 
-env
-
-wait-for-it.sh $ARTIFAKT_MYSQL_HOST:3306 --timeout=90 -- su www-data -s /bin/bash -c 'sleep 10 && cd /var/www/html/pim-community-standard && APP_ENV=dev php bin/console pim:installer:db --catalog src/Akeneo/Platform/Bundle/InstallerBundle/Resources/fixtures/minimal || :'
-
-su www-data -s /bin/bash -c './bin/console pim:user:create kbeck secretp@ssw0rd kbeck@example.com Kent Beck en_US --admin -n'
+if [ -x "/.artifakt/entrypoint.sh" ]; then
+    source /.artifakt/entrypoint.sh
+fi
 
 su www-data -s /bin/bash -c './bin/console pim:installer:check-requirements'
 
