96c340f0ed108946c2e1efe3e114583396407add
djalal <hi@djal.al>
2021-05-23 13:38:03 +0200

    akeneo5 feature check

--- akeneo/5-apache/docker-entrypoint.sh
+++ akeneo/5-apache/docker-entrypoint.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-env
+if [[ -x "/.artifakt/entrypoint.sh" ]]; then
+    source /.artifakt/entrypoint.sh
+fi
 
-wait-for-it.sh $ARTIFAKT_MYSQL_HOST:3306 --timeout=90 -- su www-data -s /bin/bash -c 'sleep 10 && cd /var/www/html/pim-community-standard && APP_ENV=dev php bin/console pim:installer:db --catalog src/Akeneo/Platform/Bundle/InstallerBundle/Resources/fixtures/minimal || :'
-
-su www-data -s /bin/bash -c './bin/console pim:user:create kbeck secretp@ssw0rd kbeck@example.com Kent Beck en_US --admin -n'
-
-su www-data -s /bin/bash -c './bin/console pim:installer:check-requirements'
+su www-data -s /bin/sh -c './bin/console pim:installer:check-requirements'
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
