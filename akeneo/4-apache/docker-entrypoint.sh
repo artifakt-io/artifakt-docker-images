8a4bb594806ee28ce3dcb8eaba3c8290a41c9bae
djalal <hi@djal.al>
2021-05-24 20:56:10 +0200

    akeneo4 feature list

--- akeneo/4-apache/docker-entrypoint.sh
+++ akeneo/4-apache/docker-entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-wait-for-it.sh $APP_DATABASE_HOST:3306 --timeout=90 -- su www-data -s /bin/sh -c 'sleep 10 && cd /var/www/html/pim-community-standard && APP_ENV=dev php bin/console pim:installer:db --catalog src/Akeneo/Platform/Bundle/InstallerBundle/Resources/fixtures/minimal || :'
-
-su www-data -s /bin/sh -c './bin/console pim:user:create kbeck secretp@ssw0rd kbeck@example.com Kent Beck en_US --admin -n'
+if [ -x "/.artifakt/entrypoint.sh" ]; then
+    source /.artifakt/entrypoint.sh
+fi
 
 su www-data -s /bin/sh -c './bin/console pim:installer:check-requirements'
 
