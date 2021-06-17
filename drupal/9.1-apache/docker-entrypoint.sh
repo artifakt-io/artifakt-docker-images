6718d0d4094f4b1c1f4c1eaf22c970691e6da483
djalal <djalal@users.noreply.github.com>
2021-06-17 14:10:35 +0200

    perf: dont slow entrypoints (#70)

--- drupal/9.1-apache/docker-entrypoint.sh
+++ drupal/9.1-apache/docker-entrypoint.sh
@@ -5,9 +5,6 @@ if [[ -x "/.artifakt/entrypoint.sh" ]]; then
     source /.artifakt/entrypoint.sh
 fi
 
-composer --no-ansi --no-interaction install --no-cache --no-progress --no-autoloader --no-scripts --no-dev && \
-composer dump-autoload
-
 # inject dotenv conf file
 [[ ! -f /data/sites/default/settings.php && -f /opt/settings.php ]] && \
   echo "first install, found a mounted conf file, copying it to default path" && \
@@ -18,9 +15,6 @@ composer dump-autoload
 [[ ! -d /data/config/sync ]] && \
   mkdir -p /data/config/sync && \
   ls -la /data/config
-
-# fix perms/owner before server launch
-chown -R www-data:www-data /data /opt/drupal/web/vendor/
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
