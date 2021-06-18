f8194a56cc576a648b8b988e099ee45758fb39fa
Aymeric Aitamer <aymeric.aitamer@gmail.com>
2021-06-18 17:25:17 +0200

    Add persistent directory cache for wordpress

--- wordpress/5-apache/etc/app-entrypoint.sh
+++ wordpress/5-apache/etc/app-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
-mkdir -p /data/wp-content/uploads && \
-  chown www-data:www-data /data/wp-content/uploads && \
-  ln -sfn /data/wp-content/uploads /var/www/html/wp-content/uploads
+mkdir -p /data/wp-content/uploads /data/wp-content/cache && \
+  chown www-data:www-data /data/wp-content/uploads /data/wp-content/cache && \
+  ln -sfn /data/wp-content/uploads /var/www/html/wp-content/uploads && \
+  ln -sfn /data/wp-content/cache /var/www/html/wp-content/cache
