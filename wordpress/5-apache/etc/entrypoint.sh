23e5f26d21d5cea119a1dcdebc4d45643f7e6b22
djalal <djalal@users.noreply.github.com>
2021-06-23 11:20:25 +0200

    one time keys generation (#76)

--- wordpress/5-apache/etc/entrypoint.sh
+++ wordpress/5-apache/etc/entrypoint.sh
@@ -5,3 +5,13 @@ mkdir -p /data/wp-content/uploads /data/wp-content/cache && \
   chown www-data:www-data /data/wp-content/uploads /data/wp-content/cache && \
   ln -sfn /data/wp-content/uploads /var/www/html/wp-content/uploads && \
   ln -sfn /data/wp-content/cache /var/www/html/wp-content/cache
+
+# generate file holding custom keys 
+[[ ! -f /data/secret-key.php ]] && \
+  echo "first install, generating secret keys" && \
+  echo "<?php " > /data/secret-key.php && \
+  curl https://api.wordpress.org/secret-key/1.1/salt >> /data/secret-key.php && \
+  chown www-data:www-data /data/secret-key.php
+
+
+
--- wordpress/5-apache/etc/wp-config.php
+++ wordpress/5-apache/etc/wp-config.php
@@ -46,14 +46,8 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         ($_ENV['AUTH_KEY'] != '') ? $_ENV['AUTH_KEY'] : '!0cYeO;9o]z}8R@Xp55eKiNU~ZtT[wsjW-(Ji=ddL$Yo*O`EXt82 r=EO1-[X}w(');
-define('SECURE_AUTH_KEY',  ($_ENV['SECURE_AUTH_KEY'] != '') ? $_ENV['SECURE_AUTH_KEY'] : '2/RGYVWR!:;z2tsuE=e84<Z-Oj>Uvm*GLzNz<53 @|gwBK$Mna%Jb8G#9D/A/xcS');
-define('LOGGED_IN_KEY',    ($_ENV['LOGGED_IN_KEY'] != '') ? $_ENV['LOGGED_IN_KEY'] : 'T3C>W`XoF<@_+[P1P-DIh(EF=Otn;hiF D%6@gaI<`4?B`U1jH7]}&@bes]5&],{');
-define('NONCE_KEY',        ($_ENV['NONCE_KEY'] != '') ? $_ENV['NONCE_KEY'] : '*hl^lPLoe<m.)w0=Nk-J3z!InR?%LD|?$ou30I0-@,{AW>|V&R-knzDR0hX+os5X');
-define('AUTH_SALT',        ($_ENV['AUTH_SALT'] != '') ? $_ENV['AUTH_SALT'] : ')*+-%6ksW-}~|nYeYf%X1uRWv4}O&m}r>P8Vp3V5A&8ly94qouQK2Nre/`_A>~Hd');
-define('SECURE_AUTH_SALT', ($_ENV['SECURE_AUTH_SALT'] != '') ? $_ENV['SECURE_AUTH_SALT'] : ',ZoFK~-:L6x(LWvI01e6>S~8zhADT(qO--(aaJ)[ aZke(&i+81K&c89%M+WjFVL');
-define('LOGGED_IN_SALT',   ($_ENV['LOGGED_IN_SALT'] != '') ? $_ENV['LOGGED_IN_SALT'] : '-2,><jcgjGm!FriM7A-$6y[R@aj.f,t1)B<$`+q`+hztch~?Ka-n%M;SS|#AbLE*');
-define('NONCE_SALT',       ($_ENV['NONCE_SALT'] != '') ? $_ENV['NONCE_SALT'] : 'E-;VUw{6?v tah]~^,dBmN`G=>+L:`OV# I:&h;]YM(lBNEr):BR3h>TP|Z@5=IU');
+
+include "/data/secret-key.php";
 
 /**#@-*/
 
