b716ca48bda845b7e20e0af8672553dfa0618d5e
djalal <djalal@users.noreply.github.com>
2021-07-01 11:54:09 +0200

    move data persistence to entrypoints (#84)
    * move data persistence to entrypoint
    
    * add tests for drupal persistent folders
    
    * add tests for wp persistent folders

--- wordpress/5-apache/docker-entrypoint.sh
+++ wordpress/5-apache/docker-entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+PERSISTENT_FOLDER_LIST=('wp-content/cache' 'wp-content/uploads' 'wp-content/backups' 'wp-content/backup-db' 'wp-content/upgrade')
+
+for persistent_folder in ${PERSISTENT_FOLDER_LIST[@]}; do
+  echo Mount $persistent_folder directory
+  rm -rf /var/www/html/$persistent_folder && \
+    mkdir -p /data/$persistent_folder && \
+    ln -sfn /data/$persistent_folder /var/www/html/$persistent_folder && \
+    chown -h www-data:www-data /var/www/html/$persistent_folder /data/$persistent_folder
+done
+
 if [[ -x "/.artifakt/entrypoint.sh" ]]; then
     source /.artifakt/entrypoint.sh
 fi
