ccf10f1fca642e3a9217802291f7bcfdd6b2983b
djalal <djalal@users.noreply.github.com>
2021-07-07 08:50:13 +0200

    fix detected error on drupal build

--- drupal/9.2-apache/docker-entrypoint.sh
+++ drupal/9.2-apache/docker-entrypoint.sh
@@ -19,7 +19,7 @@ fi
 [[ ! -f /data/sites/default/settings.php && -f /opt/settings.php ]] && \
   echo "first install, found a mounted conf file, copying it to default path" && \
   mkdir -p /data/sites/default && \
-  php -r 'include "vendor/autoload.php"; echo \Drupal\Component\Utility\Crypt::randomBytesBase64(55);' > /data/salt.txt && \
+  php -r 'include "/opt/drupal/vendor/autoload.php"; echo \Drupal\Component\Utility\Crypt::randomBytesBase64(55);' > /data/salt.txt && \
   cp /opt/settings.php /data/sites/default/settings.php && \
   chown www-data:www-data /data/sites/default/settings.php /data/salt.txt
 
