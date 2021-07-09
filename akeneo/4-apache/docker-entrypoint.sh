d2ea3cd4dc731e1545afc3cf10c06b6685427073
djalal <djalal@users.noreply.github.com>
2021-07-09 13:30:33 +0200

    logs should link to /var/log/artifakt

--- akeneo/4-apache/docker-entrypoint.sh
+++ akeneo/4-apache/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-PERSISTENT_FOLDER_LIST=("public/uploads" "var/logs")
+PERSISTENT_FOLDER_LIST=("public/uploads")
 
 for persistent_folder in ${PERSISTENT_FOLDER_LIST[@]}; do
   echo Mount $persistent_folder directory
@@ -10,6 +10,12 @@ for persistent_folder in ${PERSISTENT_FOLDER_LIST[@]}; do
     ln -sfn /data/$persistent_folder /var/www/html/pim-community-standard/$persistent_folder && \
     chown -h www-data:www-data /var/www/html/pim-community-standard/$persistent_folder /data/$persistent_folder
 done
+
+echo Mount "var/logs" directory
+rm -rf /var/www/html/pim-community-standard/var/logs && \
+  mkdir -p /var/log/artifakt && \
+  ln -sfn /var/log/artifakt /var/www/html/pim-community-standard/var/logs && \
+  chown -h www-data:www-data /var/www/html/pim-community-standard/var/logs /var/log/artifakt
 
 if [ -x "/.artifakt/entrypoint.sh" ]; then
     source /.artifakt/entrypoint.sh
