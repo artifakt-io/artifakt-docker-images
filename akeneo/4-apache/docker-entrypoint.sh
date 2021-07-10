897ab78a820fa2da8cb8829d44d928fd3ca9d3b5
djalal <djalal@users.noreply.github.com>
2021-07-10 08:55:14 +0200

    Akeneo version bump and tests (#94)
    * bump versions and tests
    
    * logs should link to /var/log/artifakt
    
    * released 5.0.40

--- akeneo/4-apache/docker-entrypoint.sh
+++ akeneo/4-apache/docker-entrypoint.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 set -e
 
-PERSISTENT_FOLDER_LIST=("public/uploads" "var/log")
+PERSISTENT_FOLDER_LIST=("public/uploads")
 
 for persistent_folder in ${PERSISTENT_FOLDER_LIST[@]}; do
   echo Mount $persistent_folder directory
-  rm -rf /var/www/html/$persistent_folder && \
+  rm -rf /var/www/html/pim-community-standard/$persistent_folder && \
     mkdir -p /data/$persistent_folder && \
-    ln -sfn /data/$persistent_folder /var/www/html/$persistent_folder && \
-    chown -h www-data:www-data /var/www/html/$persistent_folder /data/$persistent_folder
+    ln -sfn /data/$persistent_folder /var/www/html/pim-community-standard/$persistent_folder && \
+    chown -h www-data:www-data /var/www/html/pim-community-standard/$persistent_folder /data/$persistent_folder
 done
+
+echo Mount "var/logs" directory
+rm -rf /var/www/html/pim-community-standard/var/logs && \
+  mkdir -p /var/log/artifakt && \
+  ln -sfn /var/log/artifakt /var/www/html/pim-community-standard/var/logs && \
+  chown -h www-data:www-data /var/www/html/pim-community-standard/var/logs /var/log/artifakt
 
 if [ -x "/.artifakt/entrypoint.sh" ]; then
     source /.artifakt/entrypoint.sh
