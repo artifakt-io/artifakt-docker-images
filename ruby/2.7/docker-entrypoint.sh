1a01c5990a517564adcf8bf95128b004b2e54b68
djalal <djalal@users.noreply.github.com>
2021-08-18 17:37:32 +0200

    ruby2.7 patch

--- ruby/2.7/docker-entrypoint.sh
+++ ruby/2.7/docker-entrypoint.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 set -e
 
-PERSISTENT_FOLDER_LIST=("public/uploads")
-
-for persistent_folder in ${PERSISTENT_FOLDER_LIST[@]}; do
-  echo Mount $persistent_folder directory
-  rm -rf /var/www/html/$persistent_folder && \
-    mkdir -p /data/$persistent_folder && \
-    ln -sfn /data/$persistent_folder /var/www/html/$persistent_folder && \
-    chown -h www-data:www-data /var/www/html/$persistent_folder /data/$persistent_folder && \
-    ls -la /var/www/html/$persistent_folder 
-done
-
 echo Mount "var/log" directory
 rm -rf /var/www/html/var/log && \
   mkdir -p /var/log/artifakt && \
@@ -25,7 +14,7 @@ fi
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
-	set -- apache2-foreground "$@"
+	set -- ruby "$@"
 fi
 
 exec "$@"
