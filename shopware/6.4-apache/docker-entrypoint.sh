005432e194ab60cb24cb09d6b4bc45b87a094d50
Aymeric Mathéossian <aymeric.matheossian@artifakt.io>
2021-08-25 17:33:54 +0200

    Add shopware

---
+++ shopware/6.4-apache/docker-entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+echo "Creating all symbolic links"
+PERSISTENT_FOLDER_LIST=("custom/plugins" "files" "config/jwt" "public/theme" "public/media" "public/thumbnail" "public/bundles" "public/sitemap") 
+for persistent_folder in ${PERSISTENT_FOLDER_LIST[@]}; do
+  echo Mount $persistent_folder directory
+  rm -rf /var/www/html/$persistent_folder && \
+    mkdir -p /data/$persistent_folder && \
+    ln -sfn /data/$persistent_folder /var/www/html/$persistent_folder && \
+    chown -h www-data:www-data /var/www/html/$persistent_folder /data/$persistent_folder
+done
+
+ln -snf /data/.uniqueid.txt /var/www/html/
+
+until nc -z -v -w30 $DATABASE_HOST 3306
+do
+  echo "Waiting for database connection..."
+  # wait for 5 seconds before check again
+  sleep 5
+done
+
+is_installed=0
+echo "Checking if the app is already installed"
+check_if_installed=$(echo "SELECT count(*) AS TOTALNUMBEROFTABLES FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = \"shopware\";" | mysql -N -h $DATABASE_HOST -u $DATABASE_USER $DATABASE_NAME -p${DATABASE_PASSWORD})
+if [[ $check_if_installed -gt 0 && $check_if_installed != "" ]]; then
+  echo "App already installed"
+  is_installed=1
+fi
+
+if [[ -x "/.artifakt/entrypoint.sh" ]]; then
+    source /.artifakt/entrypoint.sh
+fi
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- apache2-foreground "$@"
+fi
+
+exec "$@"
