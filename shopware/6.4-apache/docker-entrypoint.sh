9f90e4d8aa3dcd981efe076a064d16e2c4b93b73
djalal <hi@djal.al>
2021-09-26 22:30:24 +0200

    shopware vendors + container tests + fixes

--- shopware/6.4-apache/docker-entrypoint.sh
+++ shopware/6.4-apache/docker-entrypoint.sh
@@ -2,23 +2,29 @@
 set -e
 
 echo "Creating all symbolic links"
-PERSISTENT_FOLDER_LIST=("custom/plugins" "files" "config/jwt" "public/theme" "public/media" "public/thumbnail" "public/bundles" "public/sitemap") 
+
+# creating main folders ain folders
+mkdir -p /var/www/html/public /var/www/html/custom /var/www/html/var
+
+PERSISTENT_FOLDER_LIST=("custom/plugins" "files" "config/jwt" "public/theme" "public/media" "public/thumbnail" "public/bundles" "public/sitemap")
 for persistent_folder in ${PERSISTENT_FOLDER_LIST[@]}; do
   echo Mount $persistent_folder directory
   rm -rf /var/www/html/$persistent_folder && \
     mkdir -p /data/$persistent_folder && \
+    mkdir -p /var/www/html/$persistent_folder && \
     ln -sfn /data/$persistent_folder /var/www/html/$persistent_folder && \
-    chown -h www-data:www-data /var/www/html/$persistent_folder /data/$persistent_folder
+    chown -h -R www-data:www-data /var/www/html/$persistent_folder /data/$persistent_folder && \
+    ls -la /var/www/html/$persistent_folder
 done
+
+echo Mount "var/logs" directory
+rm -rf /var/www/html/var/logs && \
+  mkdir -p /var/log/artifakt && \
+  ln -sfn /var/log/artifakt /var/www/html/var/logs && \
+  chown -h www-data:www-data /var/www/html/var/logs /var/log/artifakt && \
+  ls -la /var/log/artifakt
 
 ln -snf /data/.uniqueid.txt /var/www/html/
-
-until nc -z -v -w30 $DATABASE_HOST 3306
-do
-  echo "Waiting for database connection..."
-  # wait for 5 seconds before check again
-  sleep 5
-done
 
 if [[ -x "/.artifakt/entrypoint.sh" ]]; then
     source /.artifakt/entrypoint.sh
