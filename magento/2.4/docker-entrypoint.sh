2a3b801a07e0b384e890d906a82e08f39375ec33
djalal <djalal@users.noreply.github.com>
2022-03-07 16:30:07 +0100

    Update docker-entrypoint.sh

--- magento/2.4/docker-entrypoint.sh
+++ magento/2.4/docker-entrypoint.sh
@@ -6,12 +6,14 @@ PERSISTENT_FOLDER_LIST=('pub/media' 'pub/static/_cache', 'var')
 
 for persistent_folder in ${PERSISTENT_FOLDER_LIST[@]}; do
 
+  echo Init persistent folder /data/$persistent_folder
+  mkdir -p /data/$persistent_folder
+
   echo Copy modified/new files from container /var/www/html/$persistent_folder to volume /data/$persistent_folder
   cp -ur /var/www/html/$persistent_folder/* /data/$persistent_folder || true
 
   echo Link /data/$persistent_folder directory to /var/www/html/$persistent_folder
   rm -rf /var/www/html/$persistent_folder && \
-    mkdir -p /data/$persistent_folder && \
     mkdir -p /var/www/html && \    
     ln -sfn /data/$persistent_folder /var/www/html/$persistent_folder && \
     chown -h -R -L www-data:www-data /var/www/html/$persistent_folder /data/$persistent_folder
