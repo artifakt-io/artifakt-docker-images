359ffe4691c9fa8e586474f7ccb42e2219541a44
djalal <djalal@users.noreply.github.com>
2022-03-08 10:19:34 +0100

    Update docker-entrypoint.sh

--- magento/2.4/docker-entrypoint.sh
+++ magento/2.4/docker-entrypoint.sh
@@ -14,7 +14,7 @@ for persistent_folder in ${PERSISTENT_FOLDER_LIST[@]}; do
 
   echo Link /data/$persistent_folder directory to /var/www/html/$persistent_folder
   rm -rf /var/www/html/$persistent_folder && \
-    mkdir -p /var/www/html && \    
+    mkdir -p /var/www/html && \
     ln -sfn /data/$persistent_folder /var/www/html/$persistent_folder && \
     chown -h -R -L www-data:www-data /var/www/html/$persistent_folder /data/$persistent_folder
 done
