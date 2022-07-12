1b854bce7a257097a9354b1bca4be09ac3ab5d2f
Sylvain Nieuwlandt (an0rak) <sylvain.nieuwlandt@artifakt.io>
2022-07-12 20:52:28 +0200

    Force creation of /opt/drupal/web/sites/default in the container

--- drupal/9.2-apache/docker-entrypoint.sh
+++ drupal/9.2-apache/docker-entrypoint.sh
@@ -3,13 +3,12 @@ set -e
 
 PERSISTENT_FOLDER_LIST=('web/sites/default/files' 'web/sites/default/private')
 
+mkdir -p /opt/drupal/web/sites/default
 
 for persistent_folder in ${PERSISTENT_FOLDER_LIST[@]}; do
   echo Mount $persistent_folder directory
   rm -rf /opt/drupal/$persistent_folder && \
-   echo "Content of /opt/drupal/web/sites : " && ls -al /opt/drupal/web/sites/**/**  && \
    mkdir -p /data/$persistent_folder && \
-   echo "Content of /data/sites : " && ls -al /data/sites/**/** && \
    ln -sfn /data/$persistent_folder /opt/drupal/$persistent_folder && \
    chown -h www-data:www-data /opt/drupal/$persistent_folder /data/$persistent_folder
 done
