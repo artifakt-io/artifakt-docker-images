965594104e60f17123dc941ff0432db863f6bbde
Sylvain Nieuwlandt (an0rak) <sylvain.nieuwlandt@artifakt.io>
2022-07-12 16:54:15 +0200

    Add more logs

--- drupal/9.2-apache/docker-entrypoint.sh
+++ drupal/9.2-apache/docker-entrypoint.sh
@@ -3,12 +3,16 @@ set -e
 
 PERSISTENT_FOLDER_LIST=('web/sites/default/files' 'web/sites/default/private')
 
+
 for persistent_folder in ${PERSISTENT_FOLDER_LIST[@]}; do
   echo Mount $persistent_folder directory
   rm -rf /opt/drupal/$persistent_folder && \
-    mkdir -p /data/$persistent_folder && \
-    ln -sfn /data/$persistent_folder /opt/drupal/$persistent_folder && \
-    chown -h www-data:www-data /opt/drupal/$persistent_folder /data/$persistent_folder
+   ls /opt/drupal/**  && \
+   echo Creating persistant folder && \
+   mkdir -p /data/$persistent_folder && \
+   ls /data/** && \
+   ln -sfn /data/$persistent_folder /opt/drupal/$persistent_folder && \
+   chown -h www-data:www-data /opt/drupal/$persistent_folder /data/$persistent_folder
 done
 
 if [[ -x "/.artifakt/entrypoint.sh" ]]; then
