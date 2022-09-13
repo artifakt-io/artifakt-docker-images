a53aa1e311452c1cb027717f263bc6c86dddd9a2
Bruno Pinchedé <bruno.pinchede@artifakt.io>
2022-09-13 10:28:16 +0200

    quick lint

--- magento/2.4/artifakt_scripts/composer_install.sh
+++ magento/2.4/artifakt_scripts/composer_install.sh
@@ -2,4 +2,4 @@
 
 if [ -z "$ARTIFAKT_COMPOSER_VERSION" ];then ${ARTIFAKT_COMPOSER_VERSION:-"2.3.7"}; fi 
 curl -sS https://getcomposer.org/installer | \
-  php -- --version=${ARTIFAKT_COMPOSER_VERSION} --install-dir=/usr/local/bin --filename=composer
+  php -- --version="${ARTIFAKT_COMPOSER_VERSION}" --install-dir=/usr/local/bin --filename=composer
