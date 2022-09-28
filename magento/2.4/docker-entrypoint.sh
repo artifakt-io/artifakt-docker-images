60af4e55c2f626192bba192dd5601d090c761f5a
Aymeric Mathéossian <aymeric.matheossian@artifakt.io>
2022-09-28 15:31:08 +0200

    Add echo information

--- magento/2.4/docker-entrypoint.sh
+++ magento/2.4/docker-entrypoint.sh
@@ -2,6 +2,7 @@
 
 [ "$DEBUG" = "true" ] && set -x
 if [ -z $ARTIFAKT_ENTRYPOINT_OFF ]; then
+  echo "ARTIFAKT_ENTRYPOINT_OFF variable not detected, using Artifakt script artifakt_entrypoint.sh"
   source /artifakt_scripts/artifakt_entrypoint.sh
 else
   echo "ARTIFAKT_ENTRYPOINT_OFF variable detected, actions won't be run from Artifakt scripts."
@@ -9,7 +10,8 @@ fi
 
 if [ -f "/.artifakt/entrypoint.sh" ]; then
   if [[ -x "/.artifakt/entrypoint.sh" ]]; then
-      source /.artifakt/entrypoint.sh
+    echo "Entrypoint.sh script detected and executable - Starting..."
+    source /.artifakt/entrypoint.sh
   fi
 fi
 
