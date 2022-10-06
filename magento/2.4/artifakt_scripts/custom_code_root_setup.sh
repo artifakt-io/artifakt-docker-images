f91382525c12a43ab7ad083a20efcdbe511b3fff
Bruno P <104889938+Buburoi@users.noreply.github.com>
2022-10-06 10:25:29 +0200

    Add custom code root in scripts (#611)

---
+++ magento/2.4/artifakt_scripts/custom_code_root_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -n "$CUSTOM_CODE_ROOT" ]; then 
+    echo "$CUSTOM_CODE_ROOT detected";
+    echo "Moving folder to tmp";
+    mv -f /var/www/html/"$CUSTOM_CODE_ROOT" /tmp
+    ls -lah /tmp/"$CUSTOM_CODE_ROOT"
+
+    echo "Cleaning /var/www/html";
+    rm -rf /var/www/html/*
+    ls -lah /var/www/html/
+
+    echo "Moving folder to /var/www/html and settings permissions";
+    mv -f /tmp/"$CUSTOM_CODE_ROOT"/* /var/www/html/ 
+    chown -R www-data:www-data /var/www/html  
+    ls -lah /var/www/html/
+fi
