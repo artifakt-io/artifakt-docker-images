ce644853de679a7c905a4487c295c304b6dccd43
Bruno P <104889938+Buburoi@users.noreply.github.com>
2022-10-11 17:21:24 +0200

    refactor composer install process (#620)
    * refactor composer install process
    
    * refactor composer install and set up
    
    * add custom scripts for composer

--- magento/2.4-php8/artifakt_scripts/composer_install.sh
+++ magento/2.4-php8/artifakt_scripts/composer_install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-if [ -z $ARTIFAKT_COMPOSER_VERSION ]; then ARTIFAKT_COMPOSER_VERSION="2.3.7"; fi
-curl -sS https://getcomposer.org/installer | \
-  php -- --version="${ARTIFAKT_COMPOSER_VERSION}" --install-dir=/usr/local/bin --filename=composer
+COMPOSER_INSTALL_DEFAULT_ARGUMENTS="--no-cache --no-interaction --no-ansi --no-dev"
+
+if [ -f composer.json ]; then 
+    if [ -n "$ARTIFAKT_COMPOSER_INSTALL_CUSTOM_ARGUMENTS" ]; then 
+        echo "Using custom arguments for composer install: $ARTIFAKT_COMPOSER_INSTALL_CUSTOM_ARGUMENTS"
+        composer install $ARTIFAKT_COMPOSER_INSTALL_CUSTOM_ARGUMENTS
+    else
+        echo "Using default arguments for composer install: $COMPOSER_INSTALL_DEFAULT_ARGUMENTS"
+        composer install $COMPOSER_INSTALL_DEFAULT_ARGUMENTS
+    fi
+else 
+    echo "No composer.json found. It's mandatory to have this file to process the build and deploy. Please, push it to your repository." 
+fi
