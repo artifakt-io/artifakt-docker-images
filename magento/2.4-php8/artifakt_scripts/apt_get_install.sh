b3baafcba74270b41a7bd4bdef9483159b3c0d2e
Bruno Pinchedé <bruno.pinchede@artifakt.io>
2022-10-17 09:26:40 +0200

    add refactor scripts working and tested on sans complexe project

--- magento/2.4-php8/artifakt_scripts/apt_get_install.sh
+++ magento/2.4-php8/artifakt_scripts/apt_get_install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
 echo "CHECKING if more dependencies need to be installed"
 
 if [ -n "$APT_GET_INSTALL" ]; then 
-    echo "Dependebies found. Installing $APT_GET_INSTALL"
-    apt-get update && apt-get install --fix-missing -yq "$APT_GET_INSTALL" && \
+    echo "Depedencies found. Installing $APT_GET_INSTALL"
+    apt-get update --fix-missing
+    for i in $APT_GET_INSTALL; do
+        echo "Install $i"
+        sudo apt-get install -y "$i"
+    done
     rm -rf /var/lib/apt/lists/*
     echo "Install done."
 else
