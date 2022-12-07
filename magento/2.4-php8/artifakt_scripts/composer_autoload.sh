3527a5eda26987738904aef5db943da2ac00ca63
Bruno Pinchedé <bruno.pinchede@artifakt.io>
2022-12-07 09:33:32 +0100

    Remove setup:di:compile

--- magento/2.4-php8/artifakt_scripts/composer_autoload.sh
+++ magento/2.4-php8/artifakt_scripts/composer_autoload.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ -z "$ARTIFAKT_COMPOSER_AUTOLOAD_OFF" ];then
-    php bin/magento setup:di:compile
     echo "ARTIFAKT_COMPOSER_AUTOLOAD_OFF variables not detected, dump-autoload process triggered."
     composer dump-autoload --no-dev --optimize --apcu 
 fi
