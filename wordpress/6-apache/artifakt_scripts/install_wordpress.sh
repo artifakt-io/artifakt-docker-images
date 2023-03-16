4b27e27f058ed183abe2138703bd8c0f8bf8beb8
Bruno Pinchedé <bruno.pinchede@artifakt.io>
2023-03-16 06:32:21 +0100

    Wordpress 6 init

---
+++ wordpress/6-apache/artifakt_scripts/install_wordpress.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+[ "$DEBUG" = "true" ] && set -x
+
+ROOT_PATH="/var/www/html"
+
+#### We determine which version of WordPress to install.
+if [ -z "$WORDPRESS_VERSION" ];then WORDPRESS_VERSION="6.1.1"; fi
+echo -e "Wordpress version: $WORDPRESS_VERSION"
+
+if [ "$ARTIFAKT_IS_MAIN_INSTANCE" == 1 ]; then
+if [ ! -d "/data" ];then mkdir -p /data; fi
+	# Generate file holding custom keys 
+	[[ ! -f /data/secret-key.php ]] && \
+	  echo "<?php " > /data/secret-key.php && \
+	  curl https://api.wordpress.org/secret-key/1.1/salt >> /data/secret-key.php && \
+	  chown www-data:www-data /data/secret-key.php
+fi
+
+#### Creation of the .htaccess file
+
+echo -e "Creation of the .htaccess file in $ROOT_PATH"
+cat <<END > /var/www/html/.htaccess
+# BEGIN WordPress
+<IfModule mod_rewrite.c>
+RewriteEngine On
+RewriteBase /
+RewriteRule ^index\.php$ - [L]
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule . /index.php [L]
+</IfModule>
+# END WordPress
+END
+
+####
+# For the installation of WordPress there are three possibilities: 
+# 	- Either it is done by composer and in this case a composer.json and especially a composer.lock must be versioned 
+# 	- Either the repository is empty and WordPress must be installed from the official repository (https://wordpress.org)
+# 	- Or WordPress has been versioned in its entirety and there are no actions to be taken.
+###
+
+### We base ourselves on the existence or non-existence of the wp-includes directory to determine 
+### if the core of WordPress is present or not.
+
+if [ ! -d "/var/www/html/wp-includes" ];then
+	#### If the wp-includes directory does not exist, we check for the presence or absence of the composer.lock file
+	if [ -f "/var/www/html/composer.lock" ]; then
+	### If the composer.lock file is found, we proceed with the site installation using composer install
+	echo -e "composer.lock file found. Install wordpress with it."
+		su www-data -s /bin/bash -c "composer install --prefer-dist"
+	else 
+	### If we do not find a composer.lock file, we proceed using the official .tar.gz 
+	### with the default version 6.1.1 or the version provided through the custom variable $WORDPRESS_VERSION.
+		echo -e "No wp-includes folder. Get worpress core from $WORDPRESS_VERSION wordpress version."
+	    curl -o /var/www/html/wordpress.tar.gz -fL "https://wordpress.org/wordpress-$WORDPRESS_VERSION.tar.gz";
+	    tar -zxvf wordpress.tar.gz 
+		rm wordpress.tar.gz
+	fi
+	cp -R /var/www/html/wordpress/* .
+	rm -rf /var/www/html/wordpress
+fi
+
+### We grant the necessary permissions in accordance with the official documentation
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
