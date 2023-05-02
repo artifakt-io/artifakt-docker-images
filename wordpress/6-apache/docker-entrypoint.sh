6a4126355a6ac7ce131a3e53a43262a83d693d3e
Julien ANQUETIL <julien.anquetil@gmail.com>
2023-05-02 11:34:10 +0200

    Update docker-entrypoint.sh (#894)
    Add persitant folder mount

--- wordpress/6-apache/docker-entrypoint.sh
+++ wordpress/6-apache/docker-entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+PERSISTENT_FOLDER_LIST=('wp-content/cache' 'wp-content/uploads' 'wp-content/backups' 'wp-content/backup-db' 'wp-content/upgrade')
+
+for persistent_folder in ${PERSISTENT_FOLDER_LIST[@]}; do
+  echo Mount $persistent_folder directory
+  rm -rf /var/www/html/$persistent_folder && \
+    mkdir -p /data/$persistent_folder && \
+    ln -sfn /data/$persistent_folder /var/www/html/$persistent_folder && \
+    chown -h www-data:www-data /var/www/html/$persistent_folder /data/$persistent_folder
+done
+
 if [ -f "/.artifakt/entrypoint.sh" ]; then
 	chmod u+x /.artifakt/entrypoint.sh
   	echo "Entrypoint.sh script detected and executable - Starting..."
